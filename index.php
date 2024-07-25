07ed8dbd670b7aef6da9d51b2be24453b3e4df99
Mr-Goodday <krystiandzien01@gmail.com>
2024-07-25 16:22:28 +0200

    MatchMate_2.0

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <link rel="shortcut icon" href="src/img/MatchMate_logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="src/global.css">
+    <link rel="stylesheet" href="src/sections.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MatchMate</title>
+</head>
+<body>
+    <div id="scroll-viewer"></div>
+    <header>
+        <h3>
+            <img src="src/img/MatchMate_logo.png" alt="MatchMate_logo.png">
+            MatchMate
+        </h3>
+        <nav>
+            <a href="#">Home</a>
+            <a href="#">Szukaj</a>
+            <a href="#">Kontakt</a>
+            <div class="accont-buttons">
+                <a href="#"><button type="cta">Zaloguj się</button></a>
+                <a href="#"><button type="cta">Zarejestruj się</button></a>
+            </div>
+        </nav>
+    </header>
+
+    <main>
+
+        <section class="hero-section full-bg">
+            <h1>Najlepsza wyszukiwarka graczy</h1>
+            <p center="true" type="important">MatchMate pomaga graczom znaleźć osoby do wspólnego grania. Wystarczy wybrać swoje preferencje i wyszukać, a MatchMate dopasuję nam odpowiednie osoby.</p>
+            <a href=""><button>Szukaj!</button></a>
+        </section>
+
+        <section class="title-section">
+        <div class="title-container">
+          <h2>O nas</h2>
+          <p>Witaj! Nazywamy się Cors. Jesteśmy początkującym zespołem programistów z Polski, który stworzył tę stronę. Programowanie to nasza pasja. Uwielbiamy tworzyć ciekawe i przydatne strony internetowe. Pomimo, że dopiero zaczynamy, wiemy, co robimy. Wszyscy nasi pracownicy ukończyli wysokiej jakości kursy na najlepszych uczelniach. Naszym celem jest sprawienie, by internet stał się przyjaźniejszy, zwłaszcza dla najmłodszych uczestników. Chcesz wiedzieć, kto jest w naszym zespole?</p>
+          <a href=""><button type="cta">Zobacz więcej!</button></a>
+        </div>
+      </section>
+    </main>
+
+    <footer>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Szukaj</a></li>
+            <li><a href="#">Kontakt</a></li>
+        </ul>
+        <ul>
+            <li><a href="#">Zaloguj się</a></li>
+            <li><a href="#">Zarejestruj się</a></li>
+            <li><a href="#">Zapomniałem hasła</a></li>
+            <li><a href="#">Szukaj graczy</a></li>
+        </ul>
+        <img src="src/img/MatchMate_logo.png" alt="MatchMate_logo.png">
+    </footer>
+
+    <p
+      style="
+        position: absolute;
+        top: 100%;
+        margin: 0;
+        width: 100%;
+        max-width: unset;
+        padding: 4px 0;
+        background: black;
+        color: white;
+        text-align: center;
+        font-size: 16px;
+      "
+    >
+      This website is designed by
+      <a
+        href="http://goodday.great-site.net/"
+        target="_blank"
+        title="goodday.great-site.net/"
+        style="color: #69ccff"
+        >Mr.GoodDay</a
+      >.
+    </p>
+</body>
+</html>
